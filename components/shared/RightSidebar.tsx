5dfe60008fd7711e1f301d2f8de32998e2a3d7a5
Mustafa Sungur Öztürk <mustafasungur.ozturkk@gmail.com>
2023-08-30 18:46:43 +0300

    Home page was completed

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+
+function RighSidebar() {
+  const pathname = usePathname();
+  return (
+    <section className=" custom-scrollbar rightsidebar">
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">
+          Suggested Communities
+        </h3>
+      </div>
+
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">Suggested Users</h3>
+      </div>
+    </section>
+  );
+}
+
+export default RighSidebar;
